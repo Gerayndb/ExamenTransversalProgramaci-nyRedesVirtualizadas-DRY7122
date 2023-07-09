9f625fae9745ddfd099f38a3302648c29d7ad083
geraynd <127548973+Gerayndb@users.noreply.github.com>
2023-07-09 23:23:45 +0000

    Add files via upload

---
+++ Nombresintegrantes.py
@@ -0,0 +1,8 @@
+integrantes = []
+
+integrantes.append("Javier Coloma")
+integrantes.append("Geraynd Boy")
+integrantes.append("Diego Gonzalez")
+
+for integrante in integrantes:
+    print(integrante)
